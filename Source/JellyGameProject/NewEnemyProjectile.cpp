aa6635bee850fe60e5f337163e23c63b16922c56
David Heap <davidheap.DH@hotmail.co.uk>
2023-05-19 15:03:41 +0100

    fixing for build

--- Source/JellyGameProject/NewEnemyProjectile.cpp
+++ Source/JellyGameProject/NewEnemyProjectile.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "JellyGamePaperCharacter.h"
 #include "NewEnemyProjectile.h"
+#include "JellyGamePaperCharacter.h"
+
 
 // Sets default values
 ANewEnemyProjectile::ANewEnemyProjectile()
